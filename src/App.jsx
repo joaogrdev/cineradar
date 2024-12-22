6682c171fcbf32db8e166daa501502113da16106
joaogrdev <joaogabrielrsousa@gmail.com>
2024-12-22 18:45:00 -0300

    Tentativa 9 de ajustar erro de router

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,43 @@
-// import './App.css'
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-// } from "react-router-dom";
-// import Home from './Pages/Home/Home.jsx';
-// import Error from './Pages/Error/Error.jsx';
-// import MovieDetails from './Pages/MovieDetails/MovieDetails.jsx';
-// import Search from './Pages/Search/Search.jsx';
-
-// function App() {
-  
-//   const router = createBrowserRouter([
-//     {
-//       path: "/",
-//       element: <Home/>,
-//       errorElement: <Error/>
-//     },
-//     {
-//       path: "/filme/:id",
-//       element: <MovieDetails />,
-//     },
-//     {
-//       path: "/pesquisa/:filter",
-//       element: <Search />,
-//       loader: ({ params, search }) => ({ filter: params.filter, id: new URLSearchParams(search).get('q') }),
-//     }
-//   ],
-//   {
-//     basename: "/cineradar", // Define o caminho base
-//   }
-// );
-
-//   return (
-//     <RouterProvider router={router} />  
-//   )
-// }
-
-// export default App
-
-
-import './App.css';
+import './App.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import { useEffect } from 'react';
 import Home from './Pages/Home/Home.jsx';
 import Error from './Pages/Error/Error.jsx';
 import MovieDetails from './Pages/MovieDetails/MovieDetails.jsx';
 import Search from './Pages/Search/Search.jsx';
 
 function App() {
-  const router = createBrowserRouter(
-    [
-      {
-        path: "/",
-        element: <Home />,
-        errorElement: <Error />
-      },
-      {
-        path: "/filme/:id",
-        element: <MovieDetails />,
-      },
-      {
-        path: "/pesquisa/:filter",
-        element: <Search />,
-        loader: ({ params, search }) => ({
-          filter: params.filter,
-          id: new URLSearchParams(search).get('q')
-        }),
-      }
-    ],
+  
+  const router = createBrowserRouter([
     {
-      basename: "/cineradar", // Caminho base para o GitHub Pages
+      path: "/",
+      element: <Home/>,
+      errorElement: <Error/>
+    },
+    {
+      path: "/filme/:id",
+      element: <MovieDetails />,
+    },
+    {
+      path: "/pesquisa/:filter",
+      element: <Search />,
+      loader: ({ params, search }) => ({ filter: params.filter, id: new URLSearchParams(search).get('q') }),
     }
-  );
-
-  // Lida com redirecionamento vindo do 404.html
-  useEffect(() => {
-    const query = new URLSearchParams(window.location.search);
-    const redirectedPath = query.get("redirect");
-    
-    if (redirectedPath) {
-      const decodedPath = decodeURIComponent(redirectedPath);
-      // Substitui a URL no histórico sem o parâmetro `redirect`
-      window.history.replaceState({}, '', decodedPath);
-    }
-  }, []);
+  ],
+  {
+    basename: "/cineradar", // Define o caminho base
+  }
+);
 
   return (
-    <RouterProvider router={router} />
-  );
+    <RouterProvider router={router} />  
+  )
 }
 
-export default App;
+export default App
+
+
+
 
